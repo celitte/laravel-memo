3a3908518389517ac381a5e4b11ac1e712f5d3af
celitte <kob221kt@gmail.com>
2023-03-08 00:59:54 +0900

    make post time

--- resources/views/create.blade.php
+++ resources/views/create.blade.php
@@ -9,6 +9,25 @@
             <p class="card-text">
             <div class="form-group">
                 <label for="exampleFormControlTextarea1">新規作成</label>
+
+                <div class="form-group">
+  <label for="time-input">時間を入力してください:</label>
+  <select class="form-control" id="time-input" name="time" required>
+    <option value="">--選択してください--</option>
+    <option value="5">5分</option>  
+    <option value="10">10分</option>
+    <option value="15">15分</option>
+    <option value="30">30分</option>
+    <option value="45">45分</option>    
+    <option value="60">1時間</option>
+  </select>
+  <div class="invalid-feedback">
+    有効な時間単位を選択してください。
+  </div>
+</div>
+
+
+
                 <textarea class="form-control" name="content" rows="8" placeholder="メモを残しましょう"></textarea>
             </div>
             <button type="submit" class="btn btn-primary">保存！</button>
