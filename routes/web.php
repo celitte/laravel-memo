58b2110224a665a8dea0bafeadccb680dbd939a3
celitte <kob221kt@gmail.com>
2023-05-10 02:08:04 +0900

    fix web.php

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\HomeCOntroller;
+use App\Http\Controllers\HomeController;
 
 /*
 |--------------------------------------------------------------------------
@@ -23,10 +23,8 @@ Auth::routes();
 Route::get('/home', [HomeController::class, 'index'])->name('home');
 Route::post('/home', [HomeController::class, 'store'])->name('store');
 //編集画面に遷移させる
-Route::get('/detail/{id}',[HomeController::class, 'detail'])->name('detail');
+Route::get('/detail/{id}', [HomeController::class, 'detail'])->name('detail');
 // 更新処理
 Route::post('/update', [HomeController::class, 'update'])->name('update');
 //削除処理
 Route::post('/destroy', [HomeController::class, 'destroy'])->name('destroy');
-
-
